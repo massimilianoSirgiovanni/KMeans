9a69ab22c6e8bb21979c8d0582c35e260f9a49ea
Massimiliano Sirgiovanni <73539966+massimilianoSirgiovanni@users.noreply.github.com>
2022-02-08 20:11:49 +0100

    First Commit

---
+++ cmake-build-debug/CMakeFiles/Kmeans.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kmeans.dir/main.c.o"
+  "Kmeans.exe"
+  "Kmeans.pdb"
+  "libKmeans.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Kmeans.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
